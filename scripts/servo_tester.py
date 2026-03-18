fb36129642badf6f022523e08b6e540732f292f1
Nazar-Rush <nazarrush1@gmail.com>
2026-03-17 21:50:31 -0400

    tester script

--- scripts/servo_tester.py
+++ scripts/servo_tester.py
@@ -8,52 +8,66 @@
 # sudo apt update
 # sudo apt install pigpio python3-pigpio
 # sudo systemctl enable --now pigpiod
-import pigpio
-import sys
-import time
 
-MIN_PW = 500
-MAX_PW = 2500
+from gpiozero import AngularServo
+from time import sleep
+
+servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+while True:
+    servo.angle = 90
+    sleep(2)
+    servo.angle = 0
+    sleep(2)
+    servo.angle = -90
+    sleep(2)
+
+# import pigpio
+# import sys
+# import time
+
+# MIN_PW = 500
+# MAX_PW = 2500
 
 
-def angle_to_pulsewidth(angle):
-    angle = max(0, min(180, angle))
-    return int(MIN_PW + (angle / 180.0) * (MAX_PW - MIN_PW))
+# def angle_to_pulsewidth(angle):
+#     angle = max(0, min(180, angle))
+#     return int(MIN_PW + (angle / 180.0) * (MAX_PW - MIN_PW))
 
 
-if len(sys.argv) != 3:
-    print("Usage: python servo_set.py <gpio pin (18)> <angle>")
-    sys.exit(1)
+# if len(sys.argv) != 3:
+#     print("Usage: python servo_set.py <gpio pin (18)> <angle>")
+#     sys.exit(1)
 
-try:
-    pin = int(sys.argv[1])
-except ValueError:
-    print("Pin must be a integer.")
-    sys.exit(1)
+# try:
+#     pin = int(sys.argv[1])
+# except ValueError:
+#     print("Pin must be a integer.")
+#     sys.exit(1)
 
-try:
-    angle = float(sys.argv[2])
-except ValueError:
-    print("Angle must be a number.")
-    sys.exit(1)
+# try:
+#     angle = float(sys.argv[2])
+# except ValueError:
+#     print("Angle must be a number.")
+#     sys.exit(1)
 
-if not (0 <= angle <= 180):
-    print("Angle must be between 0 and 180.")
-    sys.exit(1)
+# if not (0 <= angle <= 180):
+#     print("Angle must be between 0 and 180.")
+#     sys.exit(1)
 
-pi = pigpio.pi()
-if not pi.connected:
-    print("Could not connect to pigpio daemon.")
-    sys.exit(1)
+# pi = pigpio.pi()
+# if not pi.connected:
+#     print("Could not connect to pigpio daemon.")
+#     sys.exit(1)
 
-try:
-    pw = angle_to_pulsewidth(angle)
-    pi.set_servo_pulsewidth(pin, pw)
-    print(f"Set servo to {angle} deg ({pw} us)")
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    pass
-finally:
-    pi.set_servo_pulsewidth(pin, 0)
-    pi.stop()
+# try:
+#     pw = angle_to_pulsewidth(angle)
+#     pi.set_servo_pulsewidth(pin, pw)
+#     print(f"Set servo to {angle} deg ({pw} us)")
+#     while True:
+#         time.sleep(1)
+# except KeyboardInterrupt:
+#     pass
+# finally:
+#     pi.set_servo_pulsewidth(pin, 0)
+#     pi.stop()
