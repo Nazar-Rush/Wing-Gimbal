3256fed20cebf9630c4221060e4d142e4261f2cd
Nazar-Rush <nazarrush1@gmail.com>
2026-03-17 20:04:31 -0400

    change preview size

--- src/video/av_stream_tester.py
+++ src/video/av_stream_tester.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # If picamera2 is missing, install it with:
 # sudo apt update
 # sudo apt install -y python3-picamera2
@@ -12,7 +13,7 @@ picam2 = Picamera2()
 # Full-screen-ish DRM preview on the active display
 picam2.start_preview(Preview.DRM, x=0, y=0, width=720, height=480)
 
-preview_config = picam2.create_preview_configuration({"size": (640, 480)})
+preview_config = picam2.create_preview_configuration({"size": (720, 480)})
 picam2.configure(preview_config)
 
 picam2.start()
